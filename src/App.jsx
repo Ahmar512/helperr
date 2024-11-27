3c3ae9029825889fd37c08f6ab01fbe3d731c051
Ahmar Ansari <85979277+Ahmar512@users.noreply.github.com>
2024-11-27 19:42:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import {sad, alone} from './constent/index.js'
+
+
+
+function App() {
+  const[para, setPara] = useState("Tell me what you are feelings.")
+
+  const handleSad = () =>{
+    let index = Math.floor(1000+Math.random()*1000)  % sad.length;
+    console.log(index);
+    setPara(sad[index]);
+
+  }
+  const handleAngery = () =>{
+
+  }
+  const handleLonely =()=>{
+    let index = Math.floor(1000+Math.random()*1000)  % alone.length;
+    setPara(alone[index]);
+
+  }
+
+  return (
+    <div className='h-[100vh] w-[100%] bg-[url("./assets/bg.jpg")] bg-cover bg-center flex justify-center items-center'>
+      <div className=' w-[100%]'>
+        <p id='paragraph' className='text-4xl max-sm:text-2xl max-h-[30%] text-center'>{para}</p>
+        <div className='flex flex-wrap w-full justify-center items-center mt-5 gap-3'>
+          <button className='px-5 py-3 bg-red-400 rounded-xl border-black border-2 font-serif font-bold' onClick={handleSad}>Sad</button>
+          <button className='px-5 py-3 bg-red-400 rounded-xl border-black border-2 font-serif font-bold' onClick={handleAngery}>Angery</button>
+          <button className='px-5 py-3 bg-red-400 rounded-xl border-black border-2 font-serif font-bold' onClick={handleLonely}>Alone</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/constent/index.js
@@ -0,0 +1,25 @@
+const sad = [
+    "“Allah does not burden a soul beyond that it can bear”",
+    "“Whoever puts his trust in Allah; He will be enough for Him.”",
+    "“Do not lose hope, nor be sad. You will surely be victorious if you are true believers.”",
+    "“No misfortune ever befalls except by permission of Allah. And whoever has faith in Allah - He will guide his heart. And Allah is Knowing of all things”",
+    "“O you who have believed, seek help through patience and prayer. Indeed, Allah is with the patient.”"
+]
+const alone = [
+    "“O My servants who have transgressed against themselves [by sinning], do not despair of the mercy of Allah . Indeed, Allah forgives all sins. Indeed, it is He who is the Forgiving, the Merciful.”",
+    "“Allah is the ally of those who believe. He brings them out from darknesses into the light. And those who disbelieve – their allies are Taghut. They take them out of the light into darknesses. Those are the companions of the Fire; they will abide eternally therein.”",
+    "“O mankind, indeed We have created you from male and female and made you peoples and tribes that you may know one another. Indeed, the most noble of you in the sight of Allah is the most righteous of you. Indeed, Allah is Knowing and Acquainted.”",
+    "“And We have already created man and know what his soul whispers to him, and We are closer to him than [his] jugular vein”",
+    "[And it will be said], “Indeed, this is for you a reward, and your effort has been appreciated.”",
+    "“For indeed, with hardship [will be] ease.”",
+    "“And the servants of the Most Merciful are those who walk upon the earth easily, and when the ignorant address them [harshly], they say [words of] peace”"
+
+
+]
+const angery =[
+
+]
+export {
+    sad,
+    alone,
+}
